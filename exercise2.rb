a246b534be171da00eeb75b596d940d0696f641b
Situ William <situ_william@yahoo.com>
2017-10-30 13:24:04 -0400

    finished exercise2 and added exercise3

--- exercise2.rb
+++ exercise2.rb
@@ -4,6 +4,7 @@ puts "Tax: #{(55*1.13).round(2)}"
 puts "Total: #{(55*1.13*1.15).round(2)}"
 
 puts "\nQuestion 2."
+puts "fifty" + 1.to_s
 
 puts "\nQuestion 3."
 puts "45628 x 7839 = #{45628*7839}"
---
+++ exercise3.rb
@@ -0,0 +1,7 @@
+puts "what is your name?"
+user_name = gets
+puts "whats your last name?"
+last_name = gets.chomp
+puts "your first name is #{user_name}"
+puts "your last name is #{last_name}"
+
